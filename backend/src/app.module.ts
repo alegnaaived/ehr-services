d65d6475ca330a733c00e75418857de3198a5bc8
Angela Devia <devia.angela.maria@gmail.com>
2025-04-04 10:45:06 -0500

    feature/docker: POC all deployment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,25 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { FieldMapping } from './models/fieldMapping.entity';
-import { FieldMappingController } from './controllers/fieldMapping.controller';
-import { FieldMappingService } from './services/fieldMappingService.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), 
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'db',
-      port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-      username: process.env.POSTGRES_USER || 'postgres',
-      password: process.env.POSTGRES_PASSWORD || 'postgres',
-      database: process.env.POSTGRES_DB || 'postgres',
+      url: process.env.DATABASE_URL,
       entities: [FieldMapping],
-      synchronize: true,
       autoLoadEntities: true,
+      logging: true,
+      synchronize: true,
     }),
   ],
-  controllers: [FieldMappingController],
-  providers: [FieldMappingService],
 })
 export class AppModule {}
